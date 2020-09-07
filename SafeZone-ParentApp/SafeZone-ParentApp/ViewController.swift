a6ca1b83928e57d5702c479400e5e902996da97c
Haitham Gado <haitham.gado@naqla.org>
2020-09-07 17:16:39 +0200

    first push

---
+++ SafeZone-ParentApp/SafeZone-ParentApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SafeZone-ParentApp
+//
+//  Created by Gado on 9/7/20.
+//  Copyright © 2020 Gado. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
